90e9328ab29bc25f7839b530fac0ce471cf29585
gpapp <99141920+ggpaap@users.noreply.github.com>
2022-10-09 14:35:45 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>
+        Animovies
+    </h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/CategoriasView">Categorias</RouterLink>
+      </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+
+header {
+    background-color: aqua;
+    height: 125px;
+    width: 1905px;
+
+    padding: 0;
+}
+
+h1 {
+   
+
+}
+</style>
---
+++ src/views/CategoriasView.vue
@@ -0,0 +1,7 @@
+<template>
+  a
+</template>
+
+<style>
+
+</style>
